e0c8646fac4df5484db6c87435618b0b766d4e43
Viriginia <vireleonor29@gmail.com>
2018-10-31 17:37:02 -0300

    terminada la app de siniestros

--- urls.py
+++ urls.py
@@ -4,4 +4,5 @@ from accidentes import views
 urlpatterns = [
     path('', views.AccidentesListView.as_view(), name='accidentes_list'),
     path('accidente/<int:id_accidente>', views.accidente_detail, name='accidente_detail'),
+    path('registrar_accidente', views.registrar_accidente, name='registrar_accidente'),
 ]
--- views.py
+++ views.py
@@ -1,7 +1,8 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.views.generic.list import ListView
-from accidentes.models import Accidente,Persona
+from accidentes.models import Accidente, Persona
+from datetime import datetime
 
 class AccidentesListView(ListView):
     model = Accidente
@@ -9,6 +10,17 @@ class AccidentesListView(ListView):
 def accidente_detail(request, id_accidente):
     accidente = Accidente.objects.get(pk=id_accidente)
     personas = Accidente.objects.get(pk=id_accidente).personas.all()
-    posicion = {'lat':accidente.latitud, 'long':accidente.longitud}
 
-    return render(request, 'accidentes/accidente_detail.html',{'accidente':accidente, 'personas':personas, 'posicion':posicion})
+    return render(request, 'accidentes/accidente_detail.html',{'accidente':accidente, 'personas':personas})
+
+def registrar_accidente(request):
+    temp_fecha_hora = datetime.strptime(request.GET['fecha'], '%d-%m-%YT%H:%M:%S')
+    latitud = float(request.GET['latitud'])
+    longitud = float(request.GET['longitud'])
+    accidente = Accidente.objects.create(latitud=latitud, longitud=longitud, fecha_hora=temp_fecha_hora)
+
+    for persona in request.GET['personas'].split('p'):
+        sexo = 'Masculino' if persona.split(',')[0]=='M' else 'Femenino'
+        persona = Persona.objects.create(sexo=sexo, sangre=persona.split(',')[1], edad=persona.split(',')[2])
+        accidente.personas.add(persona)
+    return HttpResponse("OK")
